d360d931386baeba77e2877d3ffef3bdde9f6a9e
darkrose <lisa@ltmnet.com>
2013-04-20 02:02:47 +1000

    increase papyrus and grass growing speeds

--- src/environment.cpp
+++ src/environment.cpp
@@ -1226,7 +1226,7 @@ void ServerEnvironment::step(float dtime)
 				}
 				/* grow junglegrass on sand near water */
 				if(n.getContent() == CONTENT_SAND) {
-					if(myrand()%500 == 0)
+					if(myrand()%200 == 0)
 					{
 						MapNode n_top1 = m_map->getNodeNoEx(p+v3s16(0,1,0));
 						MapNode n_top2 = m_map->getNodeNoEx(p+v3s16(0,2,0));
@@ -1256,7 +1256,7 @@ void ServerEnvironment::step(float dtime)
 				}
 				/* make papyrus grow near water */
 				if(n.getContent() == CONTENT_PAPYRUS) {
-					if(myrand()%500 == 0)
+					if(myrand()%100 == 0)
 					{
 						MapNode n_top1 = m_map->getNodeNoEx(p+v3s16(0,1,0));
 						MapNode n_top2 = m_map->getNodeNoEx(p+v3s16(0,2,0));
