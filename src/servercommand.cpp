e77a251d3c761d4571d3606ac6dc1139e41048d6
darkrose <lisa@ltmnet.com>
2013-12-27 13:53:46 +1000

    improved command help

--- src/servercommand.cpp
+++ src/servercommand.cpp
@@ -39,8 +39,7 @@ void cmd_me(std::wostringstream &os,
 void cmd_privs(std::wostringstream &os,
 	ServerCommandContext *ctx)
 {
-	if(ctx->parms.size() == 1)
-	{
+	if(ctx->parms.size() == 1) {
 		// Show our own real privs, without any adjustments
 		// made for admin status
 		os<<L"-!- " + narrow_to_wide(privsToString(
@@ -48,15 +47,13 @@ void cmd_privs(std::wostringstream &os,
 		return;
 	}
 
-	if((ctx->privs & PRIV_PRIVS) == 0)
-	{
+	if ((ctx->privs & PRIV_PRIVS) == 0) {
 		os<<L"-!- You don't have permission to do that";
 		return;
 	}
 
 	Player *tp = ctx->env->getPlayer(wide_to_narrow(ctx->parms[1]).c_str());
-	if(tp == NULL)
-	{
+	if (tp == NULL) {
 		os<<L"-!- No such player";
 		return;
 	}
@@ -295,8 +292,7 @@ void cmd_banunban(std::wostringstream &os, ServerCommandContext *ctx)
 void cmd_clearobjects(std::wostringstream &os,
 	ServerCommandContext *ctx)
 {
-	if((ctx->privs & PRIV_SERVER) ==0)
-	{
+	if ((ctx->privs & PRIV_SERVER) ==0) {
 		os<<L"-!- You don't have permission to do that";
 		return;
 	}
@@ -321,6 +317,82 @@ void cmd_clearobjects(std::wostringstream &os,
 	ctx->flags |= SEND_TO_OTHERS;
 }
 
+void cmd_help(std::wostringstream &os,
+	ServerCommandContext *ctx)
+{
+	u64 privs = ctx->privs;
+
+	if (ctx->parms.size() > 1) {
+		if ((privs&PRIV_SERVER) == PRIV_SERVER) {
+			if (ctx->parms[1] == L"shutdown") {
+				os<<L"-!- /#shutdown - shutdown the server";
+				return;
+			}else if (ctx->parms[1] == L"clearobjects") {
+				os<<L"-!- /clearobjects - remove active objects from the world, may take a long time";
+				return;
+			}else if (ctx->parms[1] == L"setting") {
+				os<<L"-!- /#setting <SETTING> [= VALUE] - change or view a config setting";
+				return;
+			}
+		}
+		if ((privs&PRIV_SETTIME) == PRIV_SETTIME && ctx->parms[1] == L"time") {
+			os<<L"-!- /time <0-23999> - set the game time";
+			return;
+		}
+		if ((privs&PRIV_TELEPORT) == PRIV_TELEPORT && ctx->parms[1] == L"teleport") {
+			os<<L"-!- /teleport <X>,<Y>,<Z> - teleport to coordinates <X>,<Y>,<Z>";
+			return;
+		}
+		if ((privs&PRIV_PRIVS) == PRIV_PRIVS) {
+			if (ctx->parms[1] == L"grant") {
+				os<<L"-!- /grant <PLAYERNAME> <PRIVILEGE> - grant a new privilege to a player";
+				return;
+			}else if (ctx->parms[1] == L"revoke") {
+				os<<L"-!- /revoke <PLAYERNAME> <PRIVILEGE> - remove privilege from a player";
+				return;
+			}
+		}
+		if ((privs&PRIV_BAN) == PRIV_BAN) {
+			if (ctx->parms[1] == L"ban") {
+				os<<L"-!- /ban <PLAYERNAME OR IP ADDRESS> - ban a player from the server";
+				return;
+			}else if (ctx->parms[1] == L"unban") {
+				os<<L"-!- /unban <PLAYERNAME OR IP ADDRESS> - remove a player's ban from the server";
+				return;
+			}
+		}
+		if (ctx->parms[1] == L"privs") {
+			if ((privs&PRIV_PRIVS) == PRIV_PRIVS) {
+				os<<L"-!- /privs [PLAYERNAME] - view the server privileges of a player";
+				return;
+			}
+			os<<L"-!- /privs - view your server privileges";
+			return;
+		}
+		if (ctx->parms[1] == L"status") {
+			os<<L"-!- /status - view the server's status and welcome message";
+			return;
+		}
+		if (ctx->parms[1] == L"help") {
+			os<<L"-!- /help [COMMAND] - get help for a command or a list of commands";
+			return;
+		}
+	}
+
+	os<<L"-!- Available commands: ";
+	os<<L"help status privs ";
+	if ((privs&PRIV_SERVER) == PRIV_SERVER)
+		os<<L"shutdown setting clearobjects";
+	if ((privs&PRIV_SETTIME) == PRIV_SETTIME)
+		os<<L" time";
+	if ((privs&PRIV_TELEPORT) == PRIV_TELEPORT)
+		os<<L" teleport";
+	if ((privs&PRIV_PRIVS) == PRIV_PRIVS)
+		os<<L" grant revoke";
+	if ((privs&PRIV_BAN) == PRIV_BAN)
+		os<<L" ban unban";
+}
+
 
 std::wstring processServerCommand(ServerCommandContext *ctx)
 {
@@ -328,45 +400,31 @@ std::wstring processServerCommand(ServerCommandContext *ctx)
 	std::wostringstream os(std::ios_base::binary);
 	ctx->flags = SEND_TO_SENDER;	// Default, unless we change it.
 
-	u64 privs = ctx->privs;
-
-	if(ctx->parms.size() == 0 || ctx->parms[0] == L"help")
-	{
-		os<<L"-!- Available commands: ";
-		os<<L"status privs ";
-		if(privs & PRIV_SERVER)
-			os<<L"shutdown setting ";
-		if(privs & PRIV_SETTIME)
-			os<<L" time";
-		if(privs & PRIV_TELEPORT)
-			os<<L" teleport";
-		if(privs & PRIV_PRIVS)
-			os<<L" grant revoke";
-		if(privs & PRIV_BAN)
-			os<<L" ban unban";
-	}
-	else if(ctx->parms[0] == L"status")
+	if (ctx->parms.size() == 0 || ctx->parms[0] == L"help") {
+		cmd_help(os,ctx);
+	}else if (ctx->parms[0] == L"status") {
 		cmd_status(os, ctx);
-	else if(ctx->parms[0] == L"privs")
+	}else if(ctx->parms[0] == L"privs") {
 		cmd_privs(os, ctx);
-	else if(ctx->parms[0] == L"grant" || ctx->parms[0] == L"revoke")
+	}else if(ctx->parms[0] == L"grant" || ctx->parms[0] == L"revoke") {
 		cmd_grantrevoke(os, ctx);
-	else if(ctx->parms[0] == L"time")
+	}else if(ctx->parms[0] == L"time") {
 		cmd_time(os, ctx);
-	else if(ctx->parms[0] == L"shutdown")
+	}else if(ctx->parms[0] == L"shutdown") {
 		cmd_shutdown(os, ctx);
-	else if(ctx->parms[0] == L"setting")
+	}else if(ctx->parms[0] == L"setting") {
 		cmd_setting(os, ctx);
-	else if(ctx->parms[0] == L"teleport")
+	}else if(ctx->parms[0] == L"teleport") {
 		cmd_teleport(os, ctx);
-	else if(ctx->parms[0] == L"ban" || ctx->parms[0] == L"unban")
+	}else if(ctx->parms[0] == L"ban" || ctx->parms[0] == L"unban") {
 		cmd_banunban(os, ctx);
-	else if(ctx->parms[0] == L"me")
+	}else if(ctx->parms[0] == L"me") {
 		cmd_me(os, ctx);
-	else if(ctx->parms[0] == L"clearobjects")
+	}else if(ctx->parms[0] == L"clearobjects") {
 		cmd_clearobjects(os, ctx);
-	else
-		os<<L"-!- Invalid command: " + ctx->parms[0];
+	}else{
+		os<<L"-!- Invalid command '" + ctx->parms[0] + L"', try /help";
+	}
 
 	return os.str();
 }
