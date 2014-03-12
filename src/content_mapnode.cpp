cf558f92dc5ae2a3cb49aca87d79d709354ec245
Pentium44 <cdorm245@gmail.com>
2014-03-12 18:41:57 +0800

    Modified painting crafting, added black canvas

--- src/content_mapnode.cpp
+++ src/content_mapnode.cpp
@@ -8220,7 +8220,7 @@ void content_mapnode_init()
 	{
 		u16 r[9] = {
 			CONTENT_CRAFTITEM_STICK,	CONTENT_CRAFTITEM_STICK,	CONTENT_CRAFTITEM_STICK,
-			CONTENT_CRAFTITEM_STICK, CONTENT_CRAFTITEM_DYE_WHITE, CONTENT_CRAFTITEM_STICK,
+			CONTENT_CRAFTITEM_STRING, CONTENT_CRAFTITEM_DYE_WHITE, CONTENT_CRAFTITEM_STRING,
 			CONTENT_CRAFTITEM_STICK,	CONTENT_CRAFTITEM_STICK,	CONTENT_CRAFTITEM_STICK
 		};
 		crafting::setRecipe(r,CONTENT_PAINTING_WHITE,1);
@@ -8253,7 +8253,7 @@ void content_mapnode_init()
 	{
 		u16 r[9] = {
 			CONTENT_CRAFTITEM_STICK,	CONTENT_CRAFTITEM_STICK,	CONTENT_CRAFTITEM_STICK,
-			CONTENT_CRAFTITEM_STICK, CONTENT_CRAFTITEM_DYE_RED, CONTENT_CRAFTITEM_STICK,
+			CONTENT_CRAFTITEM_STRING, CONTENT_CRAFTITEM_DYE_RED, CONTENT_CRAFTITEM_STRING,
 			CONTENT_CRAFTITEM_STICK,	CONTENT_CRAFTITEM_STICK,	CONTENT_CRAFTITEM_STICK
 		};
 		crafting::setRecipe(r,CONTENT_PAINTING_RED,1);
@@ -8286,7 +8286,7 @@ void content_mapnode_init()
 	{
 		u16 r[9] = {
 			CONTENT_CRAFTITEM_STICK,	CONTENT_CRAFTITEM_STICK,	CONTENT_CRAFTITEM_STICK,
-			CONTENT_CRAFTITEM_STICK, CONTENT_CRAFTITEM_DYE_GREEN, CONTENT_CRAFTITEM_STICK,
+			CONTENT_CRAFTITEM_STRING, CONTENT_CRAFTITEM_DYE_GREEN, CONTENT_CRAFTITEM_STRING,
 			CONTENT_CRAFTITEM_STICK,	CONTENT_CRAFTITEM_STICK,	CONTENT_CRAFTITEM_STICK
 		};
 		crafting::setRecipe(r,CONTENT_PAINTING_GREEN,1);
@@ -8319,10 +8319,43 @@ void content_mapnode_init()
 	{
 		u16 r[9] = {
 			CONTENT_CRAFTITEM_STICK,	CONTENT_CRAFTITEM_STICK,	CONTENT_CRAFTITEM_STICK,
-			CONTENT_CRAFTITEM_STICK, CONTENT_CRAFTITEM_DYE_BLUE, CONTENT_CRAFTITEM_STICK,
+			CONTENT_CRAFTITEM_STRING, CONTENT_CRAFTITEM_DYE_BLUE, CONTENT_CRAFTITEM_STRING,
 			CONTENT_CRAFTITEM_STICK,	CONTENT_CRAFTITEM_STICK,	CONTENT_CRAFTITEM_STICK
 		};
 		crafting::setRecipe(r,CONTENT_PAINTING_BLUE,1);
+	}
+	lists::add("craftguide",i);
+	lists::add("creative",i);
+	
+	i = CONTENT_PAINTING_CANVAS;
+	f = &content_features(i);
+	f->description = std::string("Blue Flower Painting");
+	f->setAllTextures("painting.png");
+	f->setTexture(4, "painting.png");
+	f->setTexture(5, "painting_canvas.png");
+	f->setInventoryTexture("painting_canvas.png");
+	f->param_type = CPT_LIGHT;
+	f->param2_type = CPT_FACEDIR_SIMPLE;
+	f->draw_type = CDT_NODEBOX;
+	f->light_propagates = true;
+	f->sunlight_propagates = true;
+	f->solidness = 0; // drawn separately, makes no faces
+	f->walkable = false;
+	//f->wall_mounted = true;
+	f->air_equivalent = true;
+	f->flammable = 1; // can be replaced by fire if the node under it is set on fire
+	f->fuel_time = 1;
+	f->dug_item = std::string("MaterialItem2 ")+itos(CONTENT_PAINTING_CANVAS)+" 1";
+	f->type = CMT_WOOD;
+	content_mapnode_nodebox_painting(f);
+	f->hardness = 0.1;
+	{
+		u16 r[9] = {
+			CONTENT_CRAFTITEM_STICK,	CONTENT_CRAFTITEM_STICK,	CONTENT_CRAFTITEM_STICK,
+			CONTENT_CRAFTITEM_STRING, CONTENT_IGNORE, CONTENT_CRAFTITEM_STRING,
+			CONTENT_CRAFTITEM_STICK,	CONTENT_CRAFTITEM_STICK,	CONTENT_CRAFTITEM_STICK
+		};
+		crafting::setRecipe(r,CONTENT_PAINTING_CANVAS,1);
 	}
 	lists::add("craftguide",i);
 	lists::add("creative",i);
